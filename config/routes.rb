82de66a75dabd60a03f3ea5cbf7f8659a7b1b232
Mohamed Said <bmscomp@gmail.com>
2011-11-12 16:08:20 +0100

    This is a sample application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 DemoApp::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ DemoApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # root :to => 'users'
 
   # See how all your routes lay out with "rake routes"
 
